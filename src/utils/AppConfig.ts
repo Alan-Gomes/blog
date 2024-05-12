5bc31af00e7067c5926237a80d804ed96cd09ed7
Alan Gomes <alangomes11@gmail.com>
2024-05-12 17:29:55 -0300

    feat: implementa titulos de página mais descritivos

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
 	site_name: "Alan's Notes",
-	title: "Alan's Notes | Typescript",
+	title: "Alan's Notes",
 	description: 'Anotações e coisas interessantes sobre Typescript',
 	author: 'Alan Gomes',
 	locale_region: 'pt-br',
